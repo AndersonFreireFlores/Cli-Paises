34d9915345e3377d838ec2b69e203b147356b884
AndersonFreireFlores <andersonfflores75@gmail.com>
2025-01-05 13:10:04 -0300

    first commit

---
+++ src/main/java/com/example/clipaises/CliPaisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.clipaises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliPaisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliPaisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clipaises/CliPaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clipaises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliPaisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
